1141f67174289a6d2280bc83d2ff2f07e989af0f
alireza sajadi <alireza.sajadi@hotmail.com>
2021-08-30 02:17:02 +0430

    Add again TC and CUCAdd TemporaryCode Entity and Clean Up Code

---
+++ DigiKala.DataAccessLayer/Entities/TemporaryCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiKala.DataAccessLayer.Entities
+{
+    public class TemporaryCode
+    {
+        public int Id { get; set; }
+        public string Identity { get; set; }
+        [Display(Name = "کد فعال سازی موبایل یا ایمیل")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [MaxLength(6, ErrorMessage = "مقدار {0} نباید بیش تر از {1} کاراکتر باشد")]
+        public string ActiveCode { get; set; }
+        public DateTime ExpireDateTime { get; set; }
+
+    }
+}
